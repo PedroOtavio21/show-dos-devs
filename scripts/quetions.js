3249b7434b15e8d7a054a2125dd820820f23d341
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-01-13 22:29:03 -0300

    Adiciona conteúdo estruturado de código html em primeiro momento, além de conteúdo do questionário em JavaScript

---
+++ scripts/quetions.js
@@ -0,0 +1,22 @@
+const questions = [
+    {
+      question: "O que é um cronograma no planejamento de projetos?",
+      options: [
+        "Lista de tarefas sem prazo definido",
+        "Organização de prazos e atividades",
+        "Documento de requisitos",
+        "Resumo do orçamento"
+      ],
+      correctAnswer: 1,
+    },
+    {
+      question: "O que significa escopo no planejamento?",
+      options: [
+        "Descrição do projeto e seus limites",
+        "Lista de tecnologias utilizadas",
+        "Plano de gestão de riscos",
+        "Documentação financeira"
+      ],
+      correctAnswer: 0,
+    },
+];
